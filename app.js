0d673547f4db62106270cf7c427eb838972b4bb3
maxumlin <maxumlin@gmail.com>
2015-12-18 20:53:33 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 var config = {
   // - Your app's id on moneypot.com
-  app_id: 881,                             // <----------------------------- EDIT ME!
+  app_id: 811,                             // <----------------------------- EDIT ME!
   // - Displayed in the navbar
   app_name: 'MegaDice',
   // - For your faucet to work, you must register your site at Recaptcha
